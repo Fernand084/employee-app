431656246491c49e86915e32338384e5f104c88e
Fernand084 <edgar.lopezr84@outlook.com>
2025-08-26 09:24:40 -0600

    use HashRouter instead of BrawserRouter on app.tsx to use with github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Users from './pages/Users';
@@ -10,37 +9,33 @@ import Departments_page from './pages/Departments_page';
 import DepartmentEmployees_page from './pages/DepartmentEmployees_page';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
-
-
 const App = () => {
-
   return (
-    <Container>
-      <Router>
+    <Router>
+      <Container>
         <Navbar className="bg-body-tertiary">
           <Nav>
-            <Nav.Link href="/">Home</Nav.Link> 
-            <Nav.Link href="/about">About</Nav.Link> 
-            <Nav.Link href="/users">Users</Nav.Link> 
-            <Nav.Link href="/employees">Employees</Nav.Link> 
-            <Nav.Link href="/departments">Departments</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/users">Users</Nav.Link>
+            <Nav.Link as={Link} to="/employees">Employees</Nav.Link>
+            <Nav.Link as={Link} to="/departments">Departments</Nav.Link>
           </Nav>
         </Navbar>
 
         <Routes>
-          <Route path = "/" element={<Home />} />
-          <Route path = "/about" element={<About />} />
-          <Route path = "/users" element={<Users />} />
-          <Route path = "/employees" element={<Employees_page />} />
-          <Route path = "/departments" element = {<Departments_page/>}/>
-          <Route path = "/employees/:id" element = {<EmployeeDetails_page/>}/>
-          <Route path = "/employees/:id/salaries" element = {<DepartmentEmployees_page/>}/>
-          <Route path = "/departments/:id/employees" element = {<DepartmentEmployees_page/>}/>
-          <Route path = "/salary/:id/top-salaries" element = {<DepartmentEmployees_page/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/employees" element={<Employees_page />} />
+          <Route path="/departments" element={<Departments_page />} />
+          <Route path="/employees/:id" element={<EmployeeDetails_page />} />
+          <Route path="/employees/:id/salaries" element={<DepartmentEmployees_page />} />
+          <Route path="/departments/:id/employees" element={<DepartmentEmployees_page />} />
+          <Route path="/salary/:id/top-salaries" element={<DepartmentEmployees_page />} />
         </Routes>
-      </Router>
-    </Container>
-    
+      </Container>
+    </Router>
   );
 };
 
